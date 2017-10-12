d236ab42d3fbbfcd1945a598751543b418d0fa5d
inyigo <inigo@wego.com>
2017-10-12 16:14:31 +0800

    resolve path using webpack

--- test/hotel-details/Client.spec.js
+++ test/hotel-details/Client.spec.js
@@ -1,6 +1,6 @@
 var sinon = require("sinon");
-var HotelSearchClient = require("../../src/hotel-search/Client");
-var HotelDetailsClient = require("../../src/hotel-details/Client");
+var HotelSearchClient = require("src/hotel-search/Client");
+var HotelDetailsClient = require("src/hotel-details/Client");
 
 describe("HotelDetailsClient", function() {
   var client;
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
   entry: {
     npm: "./src/index.js",
-    bower: "./bower_entry.js",
+    bower: "./bower_entry.js"
+  },
+  resolve: {
+    extensions: ["", ".js"],
+    modulesDirectories: ["node_modules", "./"],
+    root: [path.join(__dirname, "src")]
   },
   output: {
     path: path.join(__dirname, "build"),
     filename: "[name].bundle.js"
-  },
+  }
 };
