187168fc4bb24a1ca37ec625e75b85fabe8ffede
Tam Tran Chi <tranchitams3201@gmail.com>
2017-10-27 00:27:50 +0700

    Expose FlightSorting and FlightFiltering

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ var Api = require("./Api");
 var FlightSearchClient = require("./flight-search/Client");
 var HotelSearchClient = require("./hotel-search/Client");
 var HotelDetailsClient = require("./hotel-details/Client");
+var FlightSorting = require('./flight-search/sorting');
+var FlightFiltering = require('./flight-search/filtering');
 
 module.exports = {
-  Api: Api,
-  FlightSearchClient: FlightSearchClient,
-  HotelSearchClient: HotelSearchClient,
-  HotelDetailsClient: HotelDetailsClient
+    Api: Api,
+    FlightSearchClient: FlightSearchClient,
+    FlightSorting: FlightSorting,
+    FlightFiltering: FlightFiltering,
+    HotelSearchClient: HotelSearchClient,
+    HotelDetailsClient: HotelDetailsClient
 };
