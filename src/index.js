f8b5cc0352dbe3e6609d2cd6fdca4da94f3076a4
wilson <wilsonp.alcantara@gmail.com>
2018-04-25 15:49:00 +0800

    pretty boy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 var Api = require("./Api");
-var FlightSearchClient = require('./flight-search/Client');
-var HotelSearchClient = require('./hotel-search/Client');
-var HotelDetailsClient = require('./hotel-details/Client');
+var FlightSearchClient = require("./flight-search/Client");
+var HotelSearchClient = require("./hotel-search/Client");
+var HotelDetailsClient = require("./hotel-details/Client");
 
 module.exports = {
   Api: Api,
   FlightSearchClient: FlightSearchClient,
   HotelSearchClient: HotelSearchClient,
-  HotelDetailsClient: HotelDetailsClient,
+  HotelDetailsClient: HotelDetailsClient
 };
