c308eafab4a02d2ea1f04ff611b726681de253fc
Xavier Er <xavier_er@live.com.sg>
2018-09-11 11:35:36 +0800

    Refactor post and get function to accept request header

--- src/Api.js
+++ src/Api.js
@@ -11,134 +11,127 @@ var Api = {
   },
 
   _hotelEndpoints: {
-    searchHotelsUrl: function() {
-      return Api.getHost("v2") + "/metasearch/hotels/searches";
-    },
-    fetchHotelsUrl: function(searchId) {
+    // searchHotelsUrl: function () {
+    //   return Api.getHost("v2") + "/metasearch/hotels/searches";
+    // },
+    fetchHotelsUrl: function (searchId) {
       var path = "/metasearch/hotels/searches/" + searchId + "/results";
       return Api.getHost("v2") + path;
     },
-    searchSingleHotelUrl: function(hotelId) {
+    searchSingleHotelUrl: function (hotelId) {
       var path = "/metasearch/hotels/" + hotelId + "/searches";
       return Api.getHost("v2") + path;
     },
-    hotelDetailsUrl: function(hotelId) {
+    hotelDetailsUrl: function (hotelId) {
       return Api.getHost("v1") + "/hotels/hotels/" + hotelId;
     }
   },
 
   _flightEndpoints: {
-    searchTrips: function() {
+    searchTrips: function () {
       var path = "/metasearch/flights/searches";
       return Api.getHost("v2") + path;
     },
-    fetchTrips: function(searchId) {
+    fetchTrips: function (searchId) {
       var path = "/metasearch/flights/searches/" + searchId + "/results";
       return Api.getHost("v2") + path;
     }
   },
 
-  getHost: function(version = "v2") {
+  getHost: function (version = "v2") {
     return this.__host[this.getEnvironment()][version];
   },
 
-  setEnvironment: function(env) {
+  setEnvironment: function (env) {
     this.env = env;
   },
 
-  getEnvironment: function() {
+  getEnvironment: function () {
     return this.env || Wego.ENV || "staging";
   },
 
-  searchTrips: function(requestBody, query) {
+  searchTrips: function (requestBody, query) {
     var uri = this._flightEndpoints.searchTrips();
     return this.post(requestBody, uri, query);
   },
 
-  fetchTrips: function(searchId, query = {}) {
+  fetchTrips: function (searchId, query = {}) {
     var uri = this._flightEndpoints.fetchTrips(searchId);
     return this.get(uri, query);
   },
 
-  searchHotels: function(requestBody, query) {
-    var uri = this._hotelEndpoints.searchHotelsUrl();
-    return this.post(requestBody, uri, query);
+  searchHotels: function (searchHotelsEndpointUrl, requestBody, query, requestHeaders) {
+    // var uri = this._hotelEndpoints.searchHotelsUrl();
+    return this.post(requestBody, searchHotelsEndpointUrl, query, requestHeaders);
   },
 
-  fetchHotels: function(searchId, query = {}) {
-    var uri = this._hotelEndpoints.fetchHotelsUrl(searchId);
-    return this.get(uri, query);
+  fetchHotels: function (searchId, query, requestHeaders) {
+    let self = this;
+    let url = self._hotelEndpoints.fetchHotelsUrl(searchId);
+    return self.get(url, query || {}, requestHeaders);
   },
 
-  searchHotel: function(requestBody, query) {
+  searchHotel: function (requestBody, query) {
     var hotelId = requestBody.search.hotelId,
       uri = this._hotelEndpoints.searchSingleHotelUrl(hotelId);
     return this.post(requestBody, uri, query);
   },
 
-  fetchHotelDetails: function(hotelId, query) {
+  fetchHotelDetails: function (hotelId, query) {
     var uri = this._hotelEndpoints.hotelDetailsUrl(hotelId);
     return this.get(uri, query);
   },
 
-  fetchCities: function(query) {
+  fetchCities: function (query) {
     var uri = this.__host[this.getEnvironment()].v1 + "/places/cities";
     return this.get(uri, query);
   },
 
-  fetchAirports: function(query) {
+  fetchAirports: function (query) {
     var uri = this.__host[this.getEnvironment()].v1 + "/places/airports";
     return this.get(uri, query);
   },
 
-  post: function(requestBody, uri, query) {
-    return fetch(this.buildUrl(uri, query), {
-      credentials: "include",
+  post: function (requestBody, url, query, requestHeaders) {
+    return fetch(this.buildUrl(url, query), {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
+      credentials: "include",
+      headers: new Headers(Object.assign({}, requestHeaders, { 'Content-Type': 'application/json' })),
       body: JSON.stringify(requestBody)
-    })
-      .then(function(response) {
-        if (response.ok) {
-          return response.json();
-        } else {
-          return Promise.reject();
-        }
-      })
-      .catch(function(e) {
-        return Promise.reject(e);
-      });
+    }).then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject();
+      }
+    }).catch(function (e) {
+      return Promise.reject(e);
+    });
   },
 
-  get: function(uri, query) {
+  get: function (uri, query, requestHeaders) {
     return fetch(this.buildUrl(uri, query), {
       method: "GET",
       credentials: "include",
-      headers: {
-        "Content-Type": "application/json"
+      headers: new Headers(Object.assign({}, requestHeaders, { 'Content-Type': 'application/json' }))
+    }).then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject();
       }
-    })
-      .then(function(response) {
-        if (response.ok) {
-          return response.json();
-        } else {
-          return Promise.reject();
-        }
-      })
-      .catch(function(e) {
-        return Promise.reject(e);
-      });
+    }).catch(function (e) {
+      return Promise.reject(e);
+    });
   },
 
-  buildUrl: function(uri, query) {
+  buildUrl: function (uri, query) {
     if (query) {
       var arr = [];
       for (var key in query) {
         var value = query[key];
         if (value instanceof Array) {
-          value.forEach(function(item) {
+          value.forEach(function (item) {
             arr.push(key + "[]=" + item);
           });
         } else {
