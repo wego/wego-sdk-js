746e37b0ef0a01313b233f0011c446abfa04dc22
Keng Hong <kenghong@wego.com>
2021-03-17 18:16:28 +0800

    rates-sorting (#115)

--- src/flight-search/sorting.js
+++ src/flight-search/sorting.js
@@ -1,8 +1,10 @@
 var utils = require('../utils');
 
 module.exports = {
-  sortTrips: function(trips, sort) {
+  sortTrips: function (trips, sort, filter = {}) {
     if (!sort) return trips;
+
+    const { providerTypes, providerCodes } = filter;
 
     function getBestFare(trip) {
       if (!trip.fares[0]) return null;
@@ -14,14 +16,14 @@ module.exports = {
     }
 
     function getDepartureTime(legIndex) {
-      return function(trip) {
+      return function (trip) {
         var leg = trip.legs[legIndex];
         return leg && leg.departureTimeMinutes;
       }
     }
 
     function getArrivalTime(legIndex) {
-      return function(trip) {
+      return function (trip) {
         var leg = trip.legs[legIndex];
         return leg && (leg.arrivalTimeMinutes + leg.durationDays * 24 * 60);
       }
@@ -40,15 +42,50 @@ module.exports = {
     // changed OUTBOUND_DEPARTURE_TIME to LEG1_DEPARTURE_TIME
     //  and INBOUND_DEPARTURE_TIME to LEG2_DEPARTURE_TIME
     // Max 6 legs for now
-    for(var i=0; i<6; i++) {
-      getterMap['LEG' + (i+1) + '_DEPARTURE_TIME'] = getDepartureTime(i);
-      getterMap['LEG' + (i+1) + '_ARRIVAL_TIME'] = getArrivalTime(i);
+    for (var i = 0; i < 6; i++) {
+      getterMap['LEG' + (i + 1) + '_DEPARTURE_TIME'] = getDepartureTime(i);
+      getterMap['LEG' + (i + 1) + '_ARRIVAL_TIME'] = getArrivalTime(i);
     }
 
-    var propertyGetter = getterMap[sort.by] || function() {};
+    var propertyGetter = getterMap[sort.by] || function () { };
     var clonedTrips = utils.cloneArray(trips);
 
-    clonedTrips.sort(function(trip1, trip2) {
+    // if provider or providerCode exists, sort in ascending with fares with provider/ providerCode first for all flights
+    if (!!providerTypes && providerTypes.length > 0 || !!providerCodes && providerCodes.length > 0) {
+      for (let clonedTrip of clonedTrips) {
+        const isProviderTypes = fare => {
+          if (!!fare && !!fare.provider) {
+            const isFacilitatedBooking = providerTypes.indexOf("instant") !== -1 && fare.provider.instant;
+            const isWegoFares = providerTypes.indexOf("instant") !== -1 && fare.provider.wegoFare;
+            const hasProviderType = providerTypes.indexOf(fare.provider.type) !== -1 && !fare.provider.instant;
+            return isFacilitatedBooking || isWegoFares || hasProviderType;  
+          }
+          return false;
+        };
+
+        const isProviderCodes = fare => {
+          const providerCode = fare.providerCode || fare.provider.code;
+          return !!providerCode ? providerCodes.indexOf(providerCode) !== -1 : false;
+        }
+
+        const sortedProviderFares = [];
+        const sortedNonProviderFares = [];
+
+        for (let fare of clonedTrip.fares) {
+          if (
+            !!providerTypes && providerTypes.length > 0 && isProviderTypes(fare) ||
+            !!providerCodes && providerCodes.length > 0 && isProviderCodes(fare)) {
+            sortedProviderFares.push(fare);
+          } else {
+            sortedNonProviderFares.push(fare);
+          }
+        }
+
+        clonedTrip.fares = sortedProviderFares.concat(sortedNonProviderFares);
+      }
+    }
+
+    clonedTrips.sort(function (trip1, trip2) {
       var compareResult = utils.compare(trip1, trip2, propertyGetter, sort.order);
       if (compareResult == 0 && sort.by != 'PRICE') {
         return utils.compare(trip1, trip2, getBestFare, 'ASC');
@@ -60,14 +97,14 @@ module.exports = {
     return clonedTrips;
   },
 
-  getCheapestTrip: function(trips) {
-    return this._getBestTripBy(trips, function(betterTrip, trip) {
+  getCheapestTrip: function (trips) {
+    return this._getBestTripBy(trips, function (betterTrip, trip) {
       return betterTrip.fares[0].price.amountUsd < trip.fares[0].price.amountUsd;
     });
   },
 
-  getFastestTrip: function(trips) {
-    return this._getBestTripBy(trips, function(betterTrip, trip) {
+  getFastestTrip: function (trips) {
+    return this._getBestTripBy(trips, function (betterTrip, trip) {
       if (betterTrip.durationMinutes === trip.durationMinutes) {
         return betterTrip.fares[0].price.amountUsd < trip.fares[0].price.amountUsd;
       }
@@ -75,8 +112,8 @@ module.exports = {
     });
   },
 
-  getBestExperienceTrip: function(trips) {
-    return this._getBestTripBy(trips, function(betterTrip, trip) {
+  getBestExperienceTrip: function (trips) {
+    return this._getBestTripBy(trips, function (betterTrip, trip) {
       if (betterTrip.score === trip.score) {
         return betterTrip.fares[0].price.amountUsd < trip.fares[0].price.amountUsd;
       }
@@ -84,7 +121,7 @@ module.exports = {
     });
   },
 
-  _getBestTripBy: function(trips, isBetterFunc) {
+  _getBestTripBy: function (trips, isBetterFunc) {
     var bestTrip = trips[0];
     for (var i = 1; i < trips.length; i++) {
       if (isBetterFunc(trips[i], bestTrip)) {
