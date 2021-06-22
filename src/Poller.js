2cd324433ada7fe61e94b83c5874e11951731832
Xavier Er <xavier_er@live.com.sg>
2021-06-22 23:32:06 +0800

    Update Poller.js

--- src/Poller.js
+++ src/Poller.js
@@ -1,4 +1,4 @@
-var Poller = function(options) {
+var Poller = function (options) {
   options = options || {};
   this.initCallApi = options.initCallApi;
   this.callApi = options.callApi;
@@ -8,90 +8,85 @@ var Poller = function(options) {
 };
 
 Poller.prototype = {
-  start: function() {
+  start: function () {
     var self = this;
-    this.timer = setTimeout(function() {
+    this.timer = setTimeout(function () {
       self.pollCount++;
       self.retryCount = 0;
       self.fetch(self.initCallApi || self.callApi);
     }, 0);
   },
 
-  reset: function() {
+  reset: function () {
     clearTimeout(this.timer);
     if (this.abortLastFetch) {
       this.abortLastFetch();
     }
     this.pollCount = 0;
-    this.resultCount = 0;
     this.forceStop = false;
   },
 
-  stop: function() {
+  stop: function () {
     this.forceStop = true;
   },
 
-  isStopping: function() {
+  isStopping: function () {
     return this.forceStop;
   },
 
-  isLastPolling: function() {
-    return this.pollCount > this.pollLimit;
+  isLastPolling: function () {
+    return this.pollCount === this.pollLimit;
   },
 
-  getProgress: function() {
-    if (this.pollCount >= this.pollLimit) {
+  getProgress: function () {
+    if (this.pollCount === this.pollLimit) {
       return 100;
     }
 
-    if (this.resultCount === undefined) {
-      return this.pollCount / this.pollLimit * 100;
-    }
-    return this.pollCount / this.pollLimit * 50 + this.resultCount / 1000 * 50;
+    return (this.pollCount / this.pollLimit) * 100;
   },
 
-  handleSuccessResponse: function(response) {
+  handleSuccessResponse: function (response) {
     this.onSuccessResponse(response);
-    this.resultCount = response.count;
     this.preparePoll();
   },
 
-  preparePoll: function() {
+  preparePoll: function () {
     var self = this;
     if (this.pollCount < this.delays.length && !this.forceStop) {
-      this.timer = setTimeout(function() {
+      this.timer = setTimeout(function () {
         self.poll();
       }, this.delays[this.pollCount]);
     }
   },
 
-  poll: function() {
+  poll: function () {
     this.pollCount++;
     this.retryCount = 0;
     this.fetch(this.callApi);
   },
 
-  retry: function(callApiFn) {
+  retry: function (callApiFn) {
     if (this.retryCount < 3) {
       this.retryCount++;
       this.fetch(callApiFn);
     }
   },
 
-  fetch: function(callApiFn) {
+  fetch: function (callApiFn) {
     var self = this;
     var aborted = false;
-    this.abortLastFetch = function() {
+    this.abortLastFetch = function () {
       aborted = true;
     };
 
     callApiFn()
-      .then(function(response) {
+      .then(function (response) {
         if (!aborted) {
           self.handleSuccessResponse(response);
         }
       })
-      .catch(function() {
+      .catch(function () {
         self.retry(callApiFn);
       });
   }
